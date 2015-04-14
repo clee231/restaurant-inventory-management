2ebf754067c0f2442a04c447fe12c8a400b6e905
cwschultz88 <cwschultz88@gmail.com>
2015-04-14 16:24:44 -0500

    Added Quantity Class

---
+++ src/edu/uic/cs342/group7/rim/Quantity.java
@@ -0,0 +1,57 @@
+package edu.uic.cs342.group7.rim;
+
+import java.util.*;
+
+/**
+ * 
+ * @author
+ * Adrian Pasciak,
+ * Chase Lee,
+ * Christopher Schultz,
+ * Nerijus Gelezinis (no-show),
+ * Patrick Tam
+ *
+ */
+public class Quantity {
+  private int count = 0;
+  private Date date;
+  
+  
+  /**
+   * Returns instance count
+   * 
+   * @return count
+   */
+  public int getCount() {
+    return count;
+  }
+  
+  
+  /**
+   * Sets instance count 
+   * 
+   * @param count
+   */
+  public void setCount(int count) {
+    this.count = count;
+  }
+  
+  
+  /**
+   * Sets instance date
+   * @param date
+   */
+  public void setDate(Date date) {
+    this.date = date;
+  }
+ 
+  
+  /**
+   * Returns instance date
+   *  
+   * @return date
+   */
+  public Date getDate() {
+    return date;
+  }
+}
