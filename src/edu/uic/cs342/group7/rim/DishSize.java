ff0973eee2b70cde2549af23c9e2833f56412e41
Adrian Pasciak <pasciak487@gmail.com>
2015-04-14 16:29:54 -0500

    First Commit test

---
+++ src/edu/uic/cs342/group7/rim/DishSize.java
@@ -0,0 +1,7 @@
+package edu.uic.cs342.group7.rim;
+
+public interface DishSize {
+
+	public ArrayList<DishIngredient> getIngredients(listOfIngredients ArrayList<DishIngredient>)	
+
+}
---
+++ src/edu/uic/cs342/group7/rim/FullOrder.java
@@ -0,0 +1,9 @@
+package edu.uic.cs342.group7.rim;
+
+public class FullOrder implements DishSize {
+
+	public ArrayList<DishIngredient> getIngredients(listOfIngredients ArrayList<DishIngredient>){
+		return listOfIngredients;
+	}
+
+}
