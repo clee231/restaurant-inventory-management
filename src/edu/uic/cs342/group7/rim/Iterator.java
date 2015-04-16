2b2b0715b8c4bdf2bf18bcecaaec92d3aa11345f
cwschultz88 <cwschultz88@gmail.com>
2015-04-16 00:38:54 -0500

    Added add method to Interator Interface. Started QuantityIterator Class.

--- src/edu/uic/cs342/group7/rim/Iterator.java
+++ src/edu/uic/cs342/group7/rim/Iterator.java
@@ -1,6 +1,7 @@
 package edu.uic.cs342.group7.rim;
 
 public interface Iterator {
+  void add(Quantity q);
   boolean hasNext();
   Quantity next();
   void remove();
---
+++ src/edu/uic/cs342/group7/rim/QuantityIterator.java
@@ -0,0 +1,25 @@
+package edu.uic.cs342.group7.rim;
+
+import java.util.*;
+
+/**
+ * @author
+ * Adrian Pasciak,
+ * Chase Lee,
+ * Christopher Schultz,
+ * Nerijus Gelezinis (no-show),
+ * Patrick Tam
+ *
+ */
+public class QuantityIterator implements Iterator{
+  private ArrayList<Quantity> list = new ArrayList<Quantity>();
+  
+  
+  public void add(Quantity q) {
+    int size = list.size();
+    
+    for(int i = 0; i < size; i++) {
+      
+    }
+  }
+}
