0051a29f776cb1cb73d7d2beb6082fa9afe55562
Adrian Pasciak <pasciak487@gmail.com>
2015-04-16 21:40:48 -0500

    Added getShoppinglist() and added public Api

--- src/edu/uic/cs342/group7/rim/Api.java
+++ src/edu/uic/cs342/group7/rim/Api.java
@@ -5,11 +5,15 @@ import java.util.Date;
 import java.util.Calendar;
 
 public class Api {
-	private Observer observer;
+	private Observer observer = new Observer();
 	private OrderingSystem orderingSystem = new OrderingSystem();
 	private InventoryManager inventory = new InventoryManager(new ArrayList<Ingredient>());// Newing with empty list to avoid null pointer exception
 	private Date currentDate = new Date();
-	private Calendar c =  Calendar.getInstance();
+	private Calendar calendar =  Calendar.getInstance();
+	
+	public Api(){
+		orderingSystem.addObserver(observer);
+	}
 	
 	public boolean orderDish(String dishName){
 		return orderingSystem.orderDish(dishName);
@@ -19,10 +23,15 @@ public class Api {
 		inventory.addItems(items);
 	}
 	
+	public ArrayList<Ingredient> getShoppingList(){
+		ArrayList<Dish> dishHistory = observer.getDishesOrdered();
+		return inventory.forecast(dishHistory);
+	}
+
 	public void updateDate(){
-		c.setTime(currentDate);
-		c.add(Calendar.DATE, 1);
-		currentDate = c.getTime();		
+		calendar.setTime(currentDate);
+		calendar.add(Calendar.DATE, 1);
+		currentDate = calendar.getTime();		
 	}
 	
 }
