8c817d09d7d0d0edeb4b7f85d89279f08bb6b3ef
Chase 李 <clee231@uic.edu>
2015-04-19 17:47:36 -0500

    Attempting to bugfix.

--- src/edu/uic/cs342/group7/rim/Client.java
+++ src/edu/uic/cs342/group7/rim/Client.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
+import java.util.Iterator;
 //Java 8 routines
 //import java.io.IOException;
 //import java.nio.charset.StandardCharsets;
