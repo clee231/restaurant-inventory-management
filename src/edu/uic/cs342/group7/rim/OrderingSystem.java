f0de8d0f517303ee8445c09e8b7ef5ee2ef138be
Patrick <patricktams@gmail.com>
2015-04-18 21:53:35 -0500

    orderDish now returns a Dish

--- src/edu/uic/cs342/group7/rim/OrderingSystem.java
+++ src/edu/uic/cs342/group7/rim/OrderingSystem.java
@@ -8,7 +8,7 @@ public class OrderingSystem {
 	
 	
 	//orders the dishes... has to interact with Ingredient class
-	public boolean orderDish(String dishName){
+	public Dish orderDish(String dishName){
 		boolean dishExists = false;
 		Dish dish = new Dish();
 		
@@ -26,14 +26,16 @@ public class OrderingSystem {
 		//check if it broke out of the loop from prior, if it did, Dish was found and can continue
 		if(dishExists == false){
 			System.out.println("Here!");
-			return false;
+			return null;
 		}
 
 		//second: get dish sizes
 		ArrayList<DishIngredient> ingredients = dishSize.getIngredients(dish.getIngredients());
-		  
+		dish.setDishIngredients(ingredients);
 		//third: check if ingredients are available for quantity and if it exists
-		for(DishIngredient ing : ingredients){
+		/*for(DishIngredient ing : ingredients){
+			System.out.println(ing.getIngredient().isIngredientAvail(ing.getQuantity()));
+			System.out.println(ing.getIngredient().getName());
 			if(ing.getIngredient().isIngredientAvail(ing.getQuantity()) == false){
 				System.out.println("Here2!");
 				return false;
@@ -41,14 +43,14 @@ public class OrderingSystem {
 			/*if(ing.getQuantity() > ing.getIngredient().getTotalQuantityOfIngredient()){
 				return false;
 			}*/
-		}
+		/*}
 		
 		//here it means for sure all the ingredients are available... can now get them
 		for(DishIngredient ing : ingredients){
 			//ing.getIngredient() -> returns Ingredient.getIngredient(ing.getQuantity()) -> passes to Ingredient class to subtract number
 			ing.getIngredient().getIngredient(ing.getQuantity());
-		}
-		return true;
+		}*/
+		return dish;
 	}
 	
 	//adds an observer
